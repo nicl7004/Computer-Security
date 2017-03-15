213fede9d29405f6937f110703d662c4d361f948
Nicl7004 <nicholas.clement@colorado.edu>
2017-03-14 23:25:33 -0600

    update from last night

---
+++ lab3-network/part2.py
@@ -0,0 +1,39 @@
+
+#bind() binds the server to a specific ip and port
+#listen() puts the server into listen mode
+# accept() and close() initate and close connection to clinet
+
+# first of all import the socket library
+import socket
+
+# next create a socket object
+s = socket.socket()
+print "Socket successfully created"
+
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 12345
+
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network
+s.bind(('', port))
+print "socket binded to %s" %(port)
+
+# put the socket into listening mode
+s.listen(5)
+print "socket is listening"
+
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
+   # Establish connection with client.
+   c, addr = s.accept()
+   print 'Got connection from', addr
+
+   # send a thank you message to the client.
+   c.send('Thank you for connecting')
+   # Close the connection with the client
+   c.close()
