ef3a47157be034dc67d5432c067f5c6198a1baf9
root <root@localhost.localdomain>
2017-03-15 22:44:12 -0400

    can access correct packet and data, but nothing more

--- lab3-network/part2.py
+++ lab3-network/part2.py
@@ -1,4 +1,8 @@
 import socket
+import scapy
+import dpkt
+# import binascii
+
 ''' capture packets
 then look for http request to free.xyz
 
@@ -20,11 +24,16 @@ if number of synax recived is less than 1/3 then throw an error'''
 import socket
 
 #create an INET, raw socket
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP,)
 
 # receive a packet
 while True:
   (byte, addr) = s.recvfrom(65565)
+  print(byte,addr)
   if "54.85.9.24" in addr:
     print("got one")
-    print(byte.decode("hex"))
+
+    eth = dpkt.ethernet.unpack(byte)
+
+
+    print(eth)
